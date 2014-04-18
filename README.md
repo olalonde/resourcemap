baf0863444c16385868dd911c3755e29f1496088
olalonde <olalonde@gmail.com>
2014-04-18 17:58:55 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# express-resourceful
+
+Resourceful routing for Express 4.x
+
+## Install
+
+```
+npm install --save express-resourceful
+```
+
+## Example
+
+```javascript
+var app = require('express')(),
+  map = require('express-resourceful')(app);
+
+// Example user resource
+// In real life, you probably want one resource per file
+var resources = {
+  user: {
+    // GET /users
+    index: function (req, res) {},
+
+    // GET /users/new
+    new: function (req, res) {},
+
+    // POST /users
+    create: function (req, res) {},
+
+    // GET /users/:user
+    show: function (req, res) {},
+
+    // GET /users/:user/edit
+    edit: function (req, res) {},
+
+    // PUT /users/:user
+    update: function (req, res) {},
+
+    // DELETE /users/:user
+    destroy: function (req, res) {}
+  },
+
+  /* more resources.... */
+
+  // Singleton resource
+  account: {
+    // GET /account
+    show: function (req, res) {},
+    // GET /account/edit
+    edit: function (req, res) {},
+    // PUT /account
+    update: function (req, res) {},
+    // DELETE /account
+    destroy: function (req, res) {}
+  }
+};
+
+map.resources('user', resources.users, function (user) {
+  // Child resource (routes are prefixed with `/users/:user`)
+  // GET /users/:user/posts
+  // GET /users/:user/posts/:post
+  // etc.
+  user.resources('post', resources.posts);
+});
+
+// You can specify the plural form of a resource
+map.resources({ name: 'tx', plural: 'txs' }, resources.txs);
+
+// Singleton resource
+map.resource('account', resources.account);
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+var _ = require('underscore'),
+  inflection = require('inflection'),
+  path = require('path');
+
+function join (arr) {
+  return path.join.apply(null, arr);
+}
+
+function map (app, context) {
+  context = _.extend({
+    basepath: '/'
+  }, context);
+
+  function generate_routes (opts, resource, is_singleton, cb) {
+    if (typeof cb !== 'function')
+      cb = function () {};
+
+    if (typeof opts === 'string') {
+      opts = { name: opts };
+    }
+
+    if (!resource)
+      throw new TypeError('Error creating ' + opts.name + ' route: no resource object passed');
+
+    opts = _.extend({
+      plural: is_singleton ? '' : inflection.pluralize(opts.name),
+      id_param: is_singleton ? '' : ':' + opts.name
+    }, opts);
+
+    var collection_routes = is_singleton ? [] : [
+      [ 'get',    '',     'index' ],
+      [ 'get',    'new',  'new' ],
+      [ 'post',   '',     'create' ]
+    ].map(function (row) {
+      return [ row[0], [ opts.plural, row[1] ], row[2] ];
+    });
+
+    var instance_routes = [
+      [ 'get',    '',     'show' ],
+      [ 'get',    'edit', 'edit' ],
+      [ 'put',    '',     'update' ],
+      [ 'delete', '',     'destroy' ]
+    ].map(function (row) {
+      return [ row[0], [ opts.plural, opts.id_param, row[1] ], row[2] ];
+    });
+
+    var routes = collection_routes.concat(instance_routes).map(function (row) {
+      return [ row[0], join([ context.basepath ].concat(row[1])), row[2] ];
+    });
+
+    var valid_routes = _.filter(routes, function (route) {
+      if (typeof resource[route[2]] === 'function') return true;
+    });
+
+    _.each(valid_routes, function (route) {
+      app[route[0]](route[1], resource[route[2]]);
+    });
+
+    return valid_routes.concat(cb(map(app, {
+      parent_context: context,
+      basepath: join([ context.basepath, opts.plural, opts.id_param ])
+    })) || []);
+  }
+
+  function resource (name, resource, cb) {
+    return generate_routes(name, resource, true, cb);
+  }
+
+  function resources (name, resource, cb) {
+    return generate_routes(name, resource, false, cb);
+  }
+
+  return {
+    resource: resource,
+    resources: resources
+  };
+
+};
+
+module.exports = map;
